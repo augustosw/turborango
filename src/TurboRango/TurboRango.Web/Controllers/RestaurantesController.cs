1ee73d349a7bfef971b02647d034795c21e68e17
Augusto Schiller <augusto.wartchow@gmail.com>
2015-06-11 04:55:31 -0300

    TurboRango.Web - Implementação de CRUD com Contato e Localização - Incompleto

--- src/TurboRango/TurboRango.Web/Controllers/RestaurantesController.cs
+++ src/TurboRango/TurboRango.Web/Controllers/RestaurantesController.cs
@@ -19,7 +19,10 @@ namespace TurboRango.Web.Controllers
         // GET: Restaurantes
         public ActionResult Index()
         {
-            return View(db.Restaurantes.ToList());
+            var restaurantes = db.Restaurantes
+                .Include(x => x.Contato)
+                .Include(x => x.Localizacao);
+            return View(restaurantes.ToList());
         }
 
         // GET: Restaurantes/Details/5
@@ -29,7 +32,12 @@ namespace TurboRango.Web.Controllers
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Restaurante restaurante = db.Restaurantes.Find(id);
+
+            Restaurante restaurante = db.Restaurantes
+                .Include(x => x.Localizacao)
+                .Include(x => x.Contato)
+                .FirstOrDefault(x => x.Id == id);
+
             if (restaurante == null)
             {
                 return HttpNotFound();
@@ -48,7 +56,7 @@ namespace TurboRango.Web.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Id,Nome,Capacidade,Categoria")] Restaurante restaurante)
+        public ActionResult Create([Bind(Include = "Id,Capacidade,Nome,Categoria,Contato,Localizacao")] Restaurante restaurante)
         {
             if (ModelState.IsValid)
             {
@@ -67,7 +75,12 @@ namespace TurboRango.Web.Controllers
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Restaurante restaurante = db.Restaurantes.Find(id);
+
+            Restaurante restaurante = db.Restaurantes
+                .Include(x => x.Localizacao)
+                .Include(x => x.Contato)
+                .FirstOrDefault(x => x.Id == id);
+            
             if (restaurante == null)
             {
                 return HttpNotFound();
@@ -80,7 +93,7 @@ namespace TurboRango.Web.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Id,Nome,Capacidade,Categoria")] Restaurante restaurante)
+        public ActionResult Edit([Bind(Include = "Id,Nome,Capacidade,Categoria,Contato,Localizacao")] Restaurante restaurante)
         {
             if (ModelState.IsValid)
             {
@@ -112,6 +125,8 @@ namespace TurboRango.Web.Controllers
         public ActionResult DeleteConfirmed(int id)
         {
             Restaurante restaurante = db.Restaurantes.Find(id);
+            db.Contatos.Remove(restaurante.Contato);
+            db.Localizacoes.Remove(restaurante.Localizacao);
             db.Restaurantes.Remove(restaurante);
             db.SaveChanges();
             return RedirectToAction("Index");
