d9f9fbbdd2654d96645ffca96dac987c300cab1d
Augusto Schiller <augusto.wartchow@gmail.com>
2015-06-12 15:37:57 -0300

    TurboRango.Web - Commit Alteração

--- src/TurboRango/TurboRango.Web/Controllers/SorteiosController.cs
+++ src/TurboRango/TurboRango.Web/Controllers/SorteiosController.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TurboRango.Dominio;
+using TurboRango.Web.Models;
 
 namespace TurboRango.Web.Controllers
 {
     public class SorteiosController : Controller
     {
+        private ApplicationDbContext db = new ApplicationDbContext();
         // GET: Sorteios
         public ActionResult Index()
         {
-            ViewBag.QtdRestaurantes = 33;
+            ViewBag.QtdRestaurantes = db.Restaurantes.ToList().Count;
             return View();
         }
     }
